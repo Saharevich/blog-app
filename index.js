2162ee442531d675122317bfca77cbad5197e57c
Vladimir <vova.saharevich2014@gmail.com>
2024-02-03 13:22:50 +0300

    second commit

--- index.js
+++ index.js
@@ -91,6 +91,12 @@ function checkInputFields() {
 postBtnNode.addEventListener('click', function() {
     if (checkInputFields()) {
     const postFromUser = getPostFromUser();
+
+    if (postFromUser.title.length > TITLE_VALIDATION_LIMIT || postFromUser.text.length > TEXT_VALIDATION_LIMIT) {
+        validationPostsMassege.innerText = `Длина заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов, а длина текста поста не должна превышать ${TEXT_VALIDATION_LIMIT} символов`;
+        validationPostsMassege.classList.remove('posts__validation__hidden');
+        return;
+    }
     
     addPost(postFromUser);
 
